d8c3a2bb0ad10042d297297e419573e4e958379d
fakhri <fakhri.azhar@proton.me>
2022-11-22 06:12:17 +0700

    Adding translate_products() function

--- common_functions.py
+++ common_functions.py
@@ -22,3 +22,28 @@ def open_connection(file_path):
         return connection
     except Error as e:
         print("Connection failed")
+
+
+def translate_products(data):
+    """
+    This function is for creating dictionary of original product name and
+    product name in English.
+
+    Argument:
+        - Pandas DataFrame of the product_category_name_translation
+
+    Return:
+        - Dictionary of original product name and product name in English
+    """
+    original_product_name = data["product_category_name"]
+    product_in_english = data["product_category_name_english"]
+
+    products = dict(zip(original_product_name,  product_in_english))
+
+    for key in products:
+        if "_" in products[key]: 
+            products[key] = " ".join(products[key].split("_")).capitalize()
+        else:
+            products[key] = products[key].capitalize()
+
+    return products
