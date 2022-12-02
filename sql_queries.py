9a9f1a8aad668692085aa0d5cc1a7971577d4cde
fakhri <fakhri.azhar@proton.me>
2022-12-02 21:08:53 +0700

    Delete all SQL queryes and change the queries, except for transalation_table

--- sql_queries.py
+++ sql_queries.py
@@ -1,45 +1,31 @@
-ordered_product = """
-SELECT 
-	order_items.order_id,
-	products.product_category_name AS product_name
-FROM olist_order_items_dataset AS order_items
-LEFT JOIN olist_products_dataset AS products
-	ON order_items.product_id = products.product_id
-"""
-
-product_reviews = """
-WITH order_reviews as (
+main_query = """
+WITH order_information as (
 SELECT
 	order_items.product_id,
 	order_data.order_id,
 	reviews.review_id,
-	reviews.review_score,
-	order_data.order_status
+    order_items.seller_id,
+	order_items.price,
+	reviews.review_score
 FROM olist_order_dataset AS order_data
-LEFT JOIN olist_order_reviews_dataset AS reviews 
+LEFT JOIN olist_order_reviews_dataset AS reviews
 	ON order_data.order_id = reviews.order_id
-LEFT JOIN olist_order_items_dataset AS order_items 
+LEFT JOIN olist_order_items_dataset AS order_items
 	ON order_data.order_id = order_items.order_id
 )
 
 SELECT
-	order_reviews.order_id,
+	order_information.order_id,
+	sellers.seller_id,
 	products.product_category_name AS product_name,
-	order_reviews.review_score
-FROM olist_products_dataset AS products
-LEFT JOIN order_reviews 
-	ON products.product_id = order_reviews.product_id;
-"""
-
-sellers_sold_items = """
-SELECT
-	products.product_category_name AS product_name,
-	sellers.seller_id
-FROM olist_order_items_dataset AS order_item
-LEFT JOIN olist_products_dataset AS products 
-	ON order_item.product_id = products.product_id
-LEFT JOIN olist_sellers_dataset AS sellers 
-	ON order_item.seller_id = sellers.seller_id; 
+	order_information.price,
+	order_information.review_score
+FROM order_information
+LEFT JOIN olist_products_dataset as products 
+	ON order_information.product_id = products.product_id
+LEFT JOIN olist_sellers_dataset as sellers
+	ON order_information.seller_id = sellers.seller_id
+;
 """
 
 translation_table = """
